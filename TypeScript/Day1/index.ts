47d795572d02785d294a36710c68aee3d2e9bb65
dj-viking <anders.swedishviking@gmail.com>
2023-12-01 01:07:09 -0500

    day1-typescript

--- TypeScript/Day1/index.ts
+++ TypeScript/Day1/index.ts
@@ -1,12 +1,43 @@
-import { readFileSync } from "fs";
+import fs from "fs";
 
-const input = readFileSync(__dirname + "\\input.txt", { encoding: "utf-8" });
-const sample = readFileSync(__dirname + "\\sample.txt", { encoding: "utf-8" });
-
-let answer1 = null;
-let answer2 = null;
+const input = fs.readFileSync(__dirname + "\\input.txt", { encoding: "utf8" });
+// const input = fs.readFileSync(__dirname + "\\sample.txt", { encoding: "utf8" });
 
 (function main1() {
-    console.log("hello world", sample);
+    // console.log("hello world", input);
+
+    let sum = 0;
+    for (const line of input.split("\r\n")) {
+        // console.log(Number(line.replace(/[a-z]*/g, "")));
+        const str: string = line.replace(/[a-z]*/g, "");
+        sum += Number(`${str[0]}${str[str.length - 1]}`);
+    }
+
+    console.log(sum);
 })();
-(function main2() {})();
+(function main2() {
+    // stole from reddit the author really jebaited me on day 1
+    const data = input
+        .split("\n")
+        .map((l) => {
+            const dgs = l
+                .split("")
+                .map((c, i) => {
+                    const nxt = l.slice(i);
+                    if (nxt.startsWith("one")) return 1;
+                    if (nxt.startsWith("two")) return 2;
+                    if (nxt.startsWith("three")) return 3;
+                    if (nxt.startsWith("four")) return 4;
+                    if (nxt.startsWith("five")) return 5;
+                    if (nxt.startsWith("six")) return 6;
+                    if (nxt.startsWith("seven")) return 7;
+                    if (nxt.startsWith("eight")) return 8;
+                    if (nxt.startsWith("nine")) return 9;
+                    return parseInt(c);
+                })
+                .filter((n) => !!n);
+            return Number("" + dgs[0] + dgs.at(-1)!);
+        })
+        .reduce((a, b) => a + b, 0);
+    console.log("hello world", data);
+})();
