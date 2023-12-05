ccd265c33122171729ea1c9045f9718796bc3855
dj-viking <anders.swedishviking@gmail.com>
2023-12-04 23:33:13 -0500

    solve part 2 day 4

--- TypeScript/Day4/index.ts
+++ TypeScript/Day4/index.ts
@@ -6,20 +6,19 @@ const lines = input.split(/\r\n|\n/);
 // 20407
 // part 2 23806951
 
-// FIND A WAY TO DO IT!!!!!
 type ParseResult = {
-    cardNum: number;
+    cardNumStr: string;
     winning: number[];
     mine: number[];
 };
 (function main1() {
     const parsed: ParseResult[] = lines.map((line) => {
         const clean = line.replace(/ +/g, " ").replace("Card ", "");
-        const [cardNum, rest] = clean.split(": ");
+        const [cardNumStr, rest] = clean.split(": ");
         const [winning, mine] = rest.split(" | ");
 
         return {
-            cardNum: Number(cardNum),
+            cardNumStr,
             winning: winning.split(" ").map((s) => Number(s)),
             mine: mine.split(" ").map((s) => Number(s)),
         } as ParseResult;
@@ -38,6 +37,41 @@ type ParseResult = {
 
     console.log("part1", sum);
 })();
+
+// win more scratch cards equal to the number of winning numbers you have
 (function main2() {
-    console.log("part2", null);
+    const parsed: ParseResult[] = lines.map((line) => {
+        const clean = line.replace(/ +/g, " ").replace("Card ", "");
+        const [cardNumStr, rest] = clean.split(": ");
+        const [winning, mine] = rest.split(" | ");
+
+        return {
+            cardNumStr,
+            winning: winning.split(" ").map((s) => Number(s)),
+            mine: mine.split(" ").map((s) => Number(s)),
+        } as ParseResult;
+    });
+
+    // console.log("scratch start", scratchCards);
+
+    const matchingCounts = parsed.map((card) => {
+        const matches = card.mine.filter((c) => card.winning.includes(c));
+        return matches.length;
+    });
+
+    const cards = Array.from(lines, () => 1);
+
+    for (let i = 0; i < matchingCounts.length; i++) {
+        const winCount = matchingCounts[i];
+        for (let j = 1; j <= winCount; j++) {
+            cards[i + j] += cards[i];
+        }
+    }
+
+    // console.log("scratch end", cards);
+
+    console.log(
+        "part2",
+        cards.reduce((a, b) => a + b, 0)
+    );
 })();
