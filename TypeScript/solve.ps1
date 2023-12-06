3b155250c45c9a6c8153a156f5d81cdd9daac122
dj-viking <anders.swedishviking@gmail.com>
2023-12-05 21:22:44 -0500

    get day5 started update solve script

--- TypeScript/solve.ps1
+++ TypeScript/solve.ps1
@@ -27,11 +27,9 @@ if (-not (Test-Path ".\dist")) {
     node ./node_modules/typescript/bin/tsc -b .
     
     Copy-Item -Path "./Day$DayNumber/$file.txt" -Destination "./dist/Day$DayNumber"
-    Copy-Item -Path "./Day$DayNumber/$file.txt" -Destination "./dist/Day$DayNumber"
     node "./dist/Day$DayNumber/index.js"
 }
 else {
-    Copy-Item -Path "./Day$DayNumber/$file.txt" -Destination "./dist/Day$DayNumber"
     Copy-Item -Path "./Day$DayNumber/$file.txt" -Destination "./dist/Day$DayNumber"
     node "./dist/Day$DayNumber/index.js"
 }
